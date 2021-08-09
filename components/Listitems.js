0c07dfdbbbb537c85408734682b533f1b18610b1
pmaher1 <pmaher2037@gmail.com>
2021-08-10 03:46:14 +1000

    another change

--- components/Listitems.js
+++ components/Listitems.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // styled components
 import {
@@ -15,25 +15,46 @@ import {SwipeListView} from "react-native-swipe-list-view";
 import {Entypo} from "@expo/vector-icons";
 
 const ListItems = ({todos, setTodos}) => {
+
+    //For styling currently swiped todo row
+    const [swipedRow, setSwipedRow] = useState(null);
+
     return (
         <SwipeListView
             data={todos}
             renderItem={(data) => {
+                const RowText = data.item.key == swipedRow ? SwipedTodoText : TodoText;
                 return (
                     <ListView>
                         <>
-                            <TodoText>{data.item.title}</TodoText>
+                            <RowText>{data.item.title}</RowText>
                             <TodoDate>{data.item.date}</TodoDate>
                         </>
                     </ListView>
                 )
             }}
             renderHiddenItem={() =>{
-                <ListViewHidden>
-                    <HiddenButton>
-                        <Entypo name="trash" size={25} color={colors.secondary} />
-                    </HiddenButton>
-                </ListViewHidden>
+                return (
+                    <ListViewHidden>
+                        <HiddenButton>
+                            <Entypo name="trash" size={25} color={colors.secondary} />
+                        </HiddenButton>
+                    </ListViewHidden>)
+            }}
+            leftOpenValue={80}
+            previewRowKey={"1"}
+            previewOpenValue={80}
+            previewOpenDelay={3000}
+            disableleftSwipe={true}
+            showVerticalScrollIndicator={false}
+            style={{
+                flex: 1, paddingBottom: 30, marginBottom: 40
+            }}
+            onRowOpen={(rowKey) => {
+                setSwipedRow(rowKey);
+            }}
+            onRowClose={()=>{
+                setSwipedRow(null);
             }}
         />
     );
