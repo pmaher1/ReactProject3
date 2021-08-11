29c0d7bc3cac5604db3502c46dce73692bcc25c0
pmaher1 <pmaher2037@gmail.com>
2021-08-11 16:09:41 +1000

    Another commit because I'm scared

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 //components
@@ -8,10 +8,33 @@ import Home from "./components/Home";
 //styled components
 import {Container} from "./styles/appStyles";
 
+//async storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppLoading from 'expo-app-loading'; 
+
 export default function App() {
+  const[ready, setReady] = useState(false);
+
+  //initial todos
+  const initialTodos = [{
+    title: "Use a racket",
+    date: "Fri, 20 Jan 2021 idk the time",
+    key: "1",
+    }, {
+        title: "Get Balls",
+        date: "Fri, 20 Jan 2021 idk the time",
+        key: "2",
+    }, {
+        title: "Go to tennis",
+        date: "Fri, 20 Jan 2021 idk the time",
+        key: "3",
+    }]
+
+  const [todos, setTodos] = useState(initialTodos);
+
   return (
     <Container>
-      <Home />
+      <Home todos={todos} setTodos={setTodos}/>
       <StatusBar style="light" />
     </Container>
   );
--- components/Home.js
+++ components/Home.js
@@ -8,24 +8,7 @@ import Header from "./Header";
 import Listitems from "./Listitems";
 import InputModal from './InputModal';
 
-const Home = () => {
-
-    //initial todos
-    const initialTodos = [{
-        title: "Use a racket",
-        date: "Fri, 20 Jan 2021 idk the time",
-        key: "1",
-    }, {
-        title: "Get Balls",
-        date: "Fri, 20 Jan 2021 idk the time",
-        key: "2",
-    }, {
-        title: "Go to tennis",
-        date: "Fri, 20 Jan 2021 idk the time",
-        key: "3",
-    }]
-
-    const [todos, setTodos] = useState(initialTodos);
+const Home = ({todos, setTodos}) => {
 
     // clear all todos
     const handleClearTodos = () => {
